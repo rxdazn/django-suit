3356211a31b3ee3ea8a3e343020cc35567a7184c
Romain Deguy <rx@interaction.net.au>
2014-01-23 15:09:04 +1100

    fixed 'you have unsaved changes' dialog incorrectly displaying when using suit+FeinCMS

--- suit/static/suit/js/suit-form-confirm.js
+++ suit/static/suit/js/suit-form-confirm.js
@@ -11,6 +11,32 @@
 
 var confirmExitIfModified = (function () {
 
+    // combines all of the values of an object that has properties values as arrays
+    // e.g
+    // var template_regions = {
+    //    'base': ['main', 'sidebar'],
+    //    '2col': ['col1', 'col2']
+    // };
+    // --> ['main', 'sidebar', 'col1', 'col2', 'sidebar']
+    var mergeObjectKeys = function(obj) {
+        var values = [];
+        for (var key in obj) {
+            values = values.concat(obj[key]);
+        }
+        return values;
+    };
+
+    var templateRegions = null;
+    var feincmsActive = false;
+    
+    // FeinCMS defined global variables defined in
+    // feincms/feincms/templates/admin/feincms/_regions_js.html
+    if (typeof REGION_MAP !== 'undefined' && typeof CONTENT_NAMES != 'undefined' &&
+        typeof template_regions !== 'undefined') {
+        feincmsActive = true;
+        var templateRegions = mergeObjectKeys(template_regions);
+    }
+
     function formIsDirty(form) {
         for (var i = 0; i < form.elements.length; i++) {
             var element = form.elements[i];
@@ -33,10 +59,30 @@ var confirmExitIfModified = (function () {
                 }
             }
             else if (type == "select-one" || type == "select-multiple") {
+                // fix to prevent 'you have unsaved changes' dialog from
+                // incorrectly displaying when FeinCMS is active 
+                //
+                // below, element.options[j].selected !=  element.options[j].defaultSelected
+                // for select values in:
+                // * 'add new item' (new ContentType)
+                // * 'insert new [ContentType]' (controls under existing region ContentType blocks)
+                // * 'move to [Region]' (controls under existing region ContentType blocks)
+                // this is what incorrectly triggers the dialog
+                // we are simply ignoring them
                 for (var j = 0; j < element.options.length; j++) {
                     if (element.options[j].selected !=
                         element.options[j].defaultSelected) {
-                        return true;
+                        if (feincmsActive) {
+                            var optionValue = $(element.options[j]).attr('value');
+                            if (optionValue in CONTENT_NAMES
+                                || templateRegions.indexOf(optionValue) > -1) {
+                                continue;
+                            } else {
+                                return true;
+                            }
+                        } else {
+                            return true;
+                        }
                     }
                 }
             }
